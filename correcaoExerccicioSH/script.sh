95d23cb038966267b448ef53370a166af2c267f8
Renan Zanelato <renan.zanelato96@gmail.com>
2022-03-31 21:06:20 -0300

    Exibindo Diretorio ATUAL

--- correcaoExerccicioSH/script.sh
+++ correcaoExerccicioSH/script.sh
@@ -1,6 +1,12 @@
+#commit1
 echo "Olá, Meu nome é Renan Zanelato";
 echo "Concatenando $(whoami)";
 
+#commit2
 echo "Que dia é hoje?"
 date;
 date '+%d/%m/%Y %H:%M:%S';
+
+#commit3
+echo "Qual é meu diretorio atual?";
+pwd;
